eae5c94c7177f0069dba40e862ae5863b9a575df
jinpu <jinpu@LAPTOP-J1SUAON8>
2018-06-23 00:23:42 +0900

    最後から指定手数遡った盤面を計算する際に持ち駒と手番を更新していなかった問題を修正

--- src/main.rs
+++ src/main.rs
@@ -310,7 +310,7 @@ fn run() -> Result<(),ApplicationError> {
 						break;
 					}
 
-					let (teban, mut banmen, mc, _, mut mvs) = match position_parser.parse(&buf.split(" ").collect::<Vec<&str>>()) {
+					let (mut teban, mut banmen, mut mc, _, mut mvs) = match position_parser.parse(&buf.split(" ").collect::<Vec<&str>>()) {
 						Ok(mut position) => match position {
 							SystemEvent::Position(teban, p, n, m) => {
 								let(banmen,mc) = match p {
@@ -349,7 +349,11 @@ fn run() -> Result<(),ApplicationError> {
 
 					for m in &mvs {
 						match banmen.apply_move_none_check(&teban,&mc,m) {
-							(b,_,_) => banmen = b,
+							(b,nmc,_) => {
+								banmen = b;
+								mc = nmc;
+								teban = teban.opposite();
+							}
 						}
 					}
 
