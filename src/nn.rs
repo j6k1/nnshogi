a578611fb7ebb6eb2a7ea8d1dd59c35045b5260b
j6k1 <jinpu@s254.xrea.com>
2022-10-03 07:51:31 +0900

    Revert "オプティマイザをMomentumSGDに戻した。（学習率は0.0005に設定）"
    This reverts commit e86b02533f6cca9d4463f8654b8d0fee7d070d4f.

--- src/nn.rs
+++ src/nn.rs
@@ -10,7 +10,7 @@ use nncombinator::cuda::mem::{Alloctype, MemoryPool};
 use nncombinator::device::{DeviceCpu, DeviceGpu};
 use nncombinator::layer::{ActivationLayer, AddLayer, AddLayerTrain, AskDiffInput, BatchForwardBase, BatchTrain, DiffInput, DiffLinearLayer, ForwardAll, ForwardDiff, InputLayer, LinearLayer, LinearOutputLayer, PreTrain, TryAddLayer};
 use nncombinator::lossfunction::CrossEntropy;
-use nncombinator::optimizer::{MomentumSGD};
+use nncombinator::optimizer::{Adam};
 use nncombinator::persistence::{BinFilePersistence, Linear, Persistence, SaveToFile};
 use nncombinator::Stack;
 use rand::{prelude, Rng, SeedableRng};
@@ -302,7 +302,7 @@ pub struct Trainer<NN>
 
 	nna:NN,
 	nnb:NN,
-	optimizer:MomentumSGD<f32>,
+	optimizer:Adam<f32>,
 	nna_filename:String,
 	nnb_filename:String,
 	nnsavedir:String,
@@ -401,7 +401,7 @@ impl TrainerCreator {
 		Ok(Trainer {
 			nna:nna,
 			nnb:nnb,
-			optimizer:MomentumSGD::new(0.0005),
+			optimizer:Adam::new(),
 			nna_filename:nna_filename,
 			nnb_filename:nnb_filename,
 			nnsavedir:savedir,
